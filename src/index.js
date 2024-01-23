fc43432a7b6f19b02a9e34abe3717eab598545ea
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2024-01-23 23:02:18 +0400

    added: proptypes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        maxRating={10}
+        color="blue"
+        size={32}
+        onSetRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating maxRating={5} size={32} />
+    <StarRating
+      maxRating={5}
+      color="green"
+      colorArr={["red", "blue", "green", "orange", "grey"]}
+      size="32"
+      messages={["Terrible", "Bad", "Okay", "Good", "Perfect"]}
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
